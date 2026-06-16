472ed311f91829a8e3f868ba7375c2a074262cce
AliAnsari21 <aliaftab2106@gmail.com>
2026-06-16 14:02:20 +0530

    data transformation added

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tbframe.f_code.co_filename
+    file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="error occurred in python script name[{0}] line number[{1}] error message[{2}]".format(
     file_name,exc_tb.tb_lineno,str(error))
     return error_message
